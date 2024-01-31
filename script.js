0ba885643490da67677e71e22a4e1a1244a81078
Anton Johansson <anton9797johansson@hotmail.com>
2024-01-31 12:06:02 +0100

    RecepiePage WIP

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let categoryDiv = document.getElementById("categoryDiv");
 categoryDiv.style.gridTemplateColumns = "repeat(2, 1fr)";
+let searchDiv = document.getElementById("searchDiv");
+let categoryTitle = document.getElementById("categoryTitle");
+let recipePageDiv = document.getElementById("recipePageDiv");
 
 //fetchar alla kategorier
 fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
 .then(res => res.json())
 .then(data => {
-    console.log(data);
 
     //loopar igenom alla kategorier och lägger till titel och bild för varje kategori i min "catagoryDiv"
     data.categories.map((category) => {
@@ -67,12 +69,9 @@ function buildCategorySearchResult(parentDiv, category) {
     .then(res => res.json())
     .then(data => {
         
+        //loopar igenom alla meals från en kategori och lägger till det elementen som behövs
         data.meals.map((meal) => {
-            console.log(meal);
             
-            //fångar id av meal
-            let mealId = meal.idMeal;
-
             let li = document.createElement("li");
 
             let searchResultImg = document.createElement("img");
@@ -98,11 +97,61 @@ function buildCategorySearchResult(parentDiv, category) {
             li.appendChild(btn);
             resultUl.appendChild(li);
             parentDiv.appendChild(resultUl);
+
+            //när användare klickar på knappen så kallar vi på vår funtion som bygger upp recept
+            btn.addEventListener("click", () => {
+                //skickar med id så vi vet vilket recept vi ska visa
+                buildRecipePage(meal.idMeal);
+            })
             
         })
 
     })
 
+}
 
+//funktion som bygger recept sidan efter specifikt id
+function buildRecipePage(mealId) {
+    //gömmer vissa element och tömmer allt i categoryDiv
+    searchDiv.hidden = true;
+    categoryTitle.hidden = true;
+    categoryDiv.innerHTML = "";
+
+    fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + mealId)
+    .then(res => res.json())
+    .then(data => {
+
+        let meal = data.meals[0];
+        console.log(meal);
+
+        //arrays för alla ingredients och mesurements
+        let ingredientsArray = [];
+        let measurementArray = [];
+
+        //loopar igenom json objektet och pushar in ingredients i ingredientsArray och mesurements i mesurmentArray
+        Object.keys(meal).forEach(function(key) {
+
+            if (key.includes("Ingredient") && meal[key] != " " && meal[key] != "" && meal[key] != null){
+
+                ingredientsArray.push(meal[key]);
+
+            } else if (key.includes("Measure") && meal[key] != " " && meal[key] != "" && meal[key] != null) {
+
+                measurementArray.push(meal[key]);
+            }
+        })
+        console.log(ingredientsArray);
+        console.log(measurementArray);
+
+        let title = document.createElement("h1");
+        title.innerText = meal.strMeal;
+        let img = document.createElement("img");
+        img.src = meal.strMealThumb;
+        let ul = document.createElement("ul");
+        let li = document.createElement("li");
+        let instructions = document.createElement("p");
+        
+        
+    })
 
 }
