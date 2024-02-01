e9589e89555cd38361e5e282dfb9a0d1a2af21d8
Anton Johansson <anton9797johansson@hotmail.com>
2024-02-01 10:07:41 +0100

    recipe page done and save recipe done

--- script.js
+++ script.js
@@ -1,59 +1,72 @@
 let categoryDiv = document.getElementById("categoryDiv");
-categoryDiv.style.gridTemplateColumns = "repeat(2, 1fr)";
 let searchDiv = document.getElementById("searchDiv");
 let categoryTitle = document.getElementById("categoryTitle");
 let recipePageDiv = document.getElementById("recipePageDiv");
+let titleAndImgDiv = document.getElementById("titleAndImgDiv");
+let instructionAndBtnDiv = document.getElementById("instructionAndBtnDiv");
+let savedRecipesBtn = document.getElementById("savedRecipesBtn");
+let searchBar = document.getElementById("searchBar");
+let searchBtn = document.getElementById("searchBtn");
+startPage();
 
-//fetchar alla kategorier
-fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
-.then(res => res.json())
-.then(data => {
-
-    //loopar igenom alla kategorier och lägger till titel och bild för varje kategori i min "catagoryDiv"
-    data.categories.map((category) => {
-        let categorySelector = document.createElement("div");
-        let categoryName = document.createElement("h2");
-        let thumbNail = document.createElement("img");
-
-        categorySelector.style.backgroundColor = "#CEDEBD";
-        categorySelector.style.margin = '20px';
-        categorySelector.style.borderRadius = "5px";
-
-        thumbNail.src = category.strCategoryThumb;
-        thumbNail.style.marginTop = "10px";
-
-        categoryName.innerText = category.strCategory;
-
-        categorySelector.appendChild(thumbNail);
-        categorySelector.appendChild(categoryName);
-        categoryDiv.appendChild(categorySelector)
-
-        //lägger till en eventlistner till varje div så vi kan klicka och välja rätt kategori
-        categorySelector.addEventListener("click", () => {
-            
-            //fångar vilken kategori användaren klickat på
-            let selectedCategory = category.strCategory;
-
-            //rensar diven när användare klickat på en katogori
-            categoryDiv.innerHTML = ""
-
-            buildCategorySearchResult(categoryDiv, selectedCategory);
-
-        })
-        //lägger till effekt när man har muspekaren på diven så man ska förstå att det är en knapp
-        categorySelector.addEventListener("mouseover", () => {
-            categorySelector.style.backgroundColor = "#435334";
-            categorySelector.style.boxShadow = "10px 20px 30px black"
-        })
-        //tar bort effekten igen när muspekaren inte är på diven
-        categorySelector.addEventListener("mouseout", () => {
+//funktion för att skapa startsidan
+function startPage() {
+    searchDiv.hidden = false;
+    categoryTitle.hidden = false;
+    titleAndImgDiv.hidden = true;
+    categoryDiv.style.gridTemplateColumns = "repeat(2, 1fr)";
+    //fetchar alla kategorier
+    fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+    .then(res => res.json())
+    .then(data => {
+    
+        //loopar igenom alla kategorier och lägger till titel och bild för varje kategori i min "catagoryDiv"
+        data.categories.map((category) => {
+            let categorySelector = document.createElement("div");
+            let categoryName = document.createElement("h2");
+            let thumbNail = document.createElement("img");
+    
             categorySelector.style.backgroundColor = "#CEDEBD";
-            categorySelector.style.boxShadow = "";
+            categorySelector.style.margin = '20px';
+            categorySelector.style.borderRadius = "5px";
+    
+            thumbNail.src = category.strCategoryThumb;
+            thumbNail.style.marginTop = "10px";
+    
+            categoryName.innerText = category.strCategory;
+    
+            categorySelector.appendChild(thumbNail);
+            categorySelector.appendChild(categoryName);
+            categoryDiv.appendChild(categorySelector)
+    
+            //lägger till en eventlistner till varje div så vi kan klicka och välja rätt kategori
+            categorySelector.addEventListener("click", () => {
+                
+                //fångar vilken kategori användaren klickat på
+                let selectedCategory = category.strCategory;
+    
+                //rensar diven när användare klickat på en katogori
+                categoryDiv.innerHTML = ""
+    
+                buildCategorySearchResult(categoryDiv, selectedCategory);
+    
+            })
+            //lägger till effekt när man har muspekaren på diven så man ska förstå att det är en knapp
+            categorySelector.addEventListener("mouseover", () => {
+                categorySelector.style.backgroundColor = "#435334";
+                categorySelector.style.boxShadow = "10px 20px 30px black"
+            })
+            //tar bort effekten igen när muspekaren inte är på diven
+            categorySelector.addEventListener("mouseout", () => {
+                categorySelector.style.backgroundColor = "#CEDEBD";
+                categorySelector.style.boxShadow = "";
+            })
         })
+    
+    
     })
+}
 
-
-})
 
 
 //funktion som skriver ut resultat av vald kategori
@@ -116,6 +129,7 @@ function buildRecipePage(mealId) {
     searchDiv.hidden = true;
     categoryTitle.hidden = true;
     categoryDiv.innerHTML = "";
+    titleAndImgDiv.hidden = false;
 
     fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + mealId)
     .then(res => res.json())
@@ -140,18 +154,87 @@ function buildRecipePage(mealId) {
                 measurementArray.push(meal[key]);
             }
         })
-        console.log(ingredientsArray);
-        console.log(measurementArray);
 
         let title = document.createElement("h1");
         title.innerText = meal.strMeal;
+
         let img = document.createElement("img");
         img.src = meal.strMealThumb;
+        img.style.width = "450px";
+
+        let btn = document.createElement("button");
+        btn.innerText = "Save This Recipe"
+
+        let commentInput = document.createElement("input");
+        commentInput.placeholder = "This comment will also be saved...";
+
         let ul = document.createElement("ul");
-        let li = document.createElement("li");
+        let ingreTitle = document.createElement("h2")
+        ingreTitle.innerText = "Ingredients"
+        ul.appendChild(ingreTitle);
+
+        //loopar igenom array och lägger till ingredients och mesurments i ul 
+        for(i = 0; i < ingredientsArray.length; i++) {
+            let li = document.createElement("li");
+            li.innerText = ingredientsArray[i] + " (Measurement: " + measurementArray[i] + ")";
+            ul.appendChild(li);
+        }
+
         let instructions = document.createElement("p");
+        instructions.innerText = meal.strInstructions;
+
+        let homeBtn = document.createElement("button");
+        homeBtn.innerText = "Go Back"
         
-        
+
+        titleAndImgDiv.appendChild(homeBtn);
+        titleAndImgDiv.appendChild(title);
+        titleAndImgDiv.appendChild(img);
+        titleAndImgDiv.appendChild(instructions);
+        instructionAndBtnDiv.appendChild(ul);
+        instructionAndBtnDiv.appendChild(commentInput);
+        instructionAndBtnDiv.appendChild(btn);
+
+        //knapp för att spara recipe
+        btn.addEventListener("click", () => {
+            saveRecipe(meal.idMeal, commentInput.value);
+        })
+
+        //för att gå tillbaka till startsidan
+        homeBtn.addEventListener("click", () => {
+            title.remove();
+            img.remove();
+            instructions.remove();
+            ul.remove();
+            btn.remove();
+            homeBtn.remove();
+            commentInput.remove();
+            startPage();
+        })
+
     })
 
 }
+
+
+//funktion för att spara recipe i databasen
+function saveRecipe(mealId, comment) {
+    
+    let mealData = {
+        id: mealId,
+        comment: comment,
+    };
+    fetch("http://localhost:8080/meal", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(mealData),
+    })
+    .then(res => res.json())
+    .then(data => {
+        alert("Recipe Saved")
+    });
+    
+}
+
